85a39ec8779d404b7d58d7aff24d81bd732d7be4
Brendan Bosman <bhbosman@gmail.com>
2022-06-03 14:16:11 +0200

    refactor

--- internal/wsCurrencyDialer/ProvideDialer.go
+++ internal/wsCurrencyDialer/ProvideDialer.go
@@ -34,36 +34,21 @@ func ProvideDialer(
 				FxCurrencyAggregationRegistration string                         `name:"Polygon-io.WS.FX.Registration.CA"`
 				NetAppFuncInParams                common.NetAppFuncInParams
 			}) messages.CreateAppCallback {
-				fxOptions := fx.Options(
-					fx.Provide(fx.Annotated{Name: "Polygon", Target: func() TickersService.ITickersService { return params.TickersService }}),
-					fx.Provide(fx.Annotated{Name: "Polygon-io.API.Key", Target: model.CreateStringContext(params.ApiKey)}),
-					fx.Provide(fx.Annotated{Name: "Polygon-io.WS.FX.Registration.C", Target: model.CreateStringContext(params.FxCurrencyAggregationRegistration)}),
-					fx.Provide(fx.Annotated{Name: "Polygon-io.WS.FX.Registration.CA", Target: model.CreateStringContext(params.FxCurrencyAggregationRegistration)}),
-					fx.Provide(
-						fx.Annotated{
-							Target: func(params struct {
-								fx.In
-								TickersService                    TickersService.ITickersService `name:"Polygon"`
-								ApiKey                            string                         `name:"Polygon-io.API.Key"`
-								FxCurrencyRegistration            string                         `name:"Polygon-io.WS.FX.Registration.C"`
-								FxCurrencyAggregationRegistration string                         `name:"Polygon-io.WS.FX.Registration.CA"`
-							}) intf.ConnectionReactorFactoryCallback {
-								return func() (intf.IConnectionReactorFactory, error) {
-									return NewConnectionReactorFactory(
-										crfName,
-										params.ApiKey,
-										params.FxCurrencyRegistration,
-										params.FxCurrencyAggregationRegistration,
-										params.TickersService), nil
-								}
-							},
-						}))
 				f := netDial.NewNetDialApp(
-					fmt.Sprintf("goPolygon-io Dialer"), serviceIdentifier, serviceDependentOn, fxOptions,
+					fmt.Sprintf("goPolygon-io Dialer"),
+					serviceIdentifier,
+					serviceDependentOn,
 					fmt.Sprintf("goPolygon-io Dialer"),
 					fmt.Sprintf("wss://socket.polygon.io:443/forex"),
 					common.WebSocketName,
-					nil,
+					func() (intf.IConnectionReactorFactory, error) {
+						return NewConnectionReactorFactory(
+							crfName,
+							params.ApiKey,
+							params.FxCurrencyRegistration,
+							params.FxCurrencyAggregationRegistration,
+							params.TickersService), nil
+					},
 					netDial.MaxConnectionsSetting(1),
 					//netDial.UserContextValue(option),
 					netDial.CanDial(settings.canDial...))
